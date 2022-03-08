7a6aa3c8a8f03d3fd0c2419d2ed89beb93a78745
Michael R Ryan <c00239510@itcarlow.ie>
2022-03-08 21:56:24 +0000

    Began implementing a GraphQL interface

---
+++ addons/godot-enjin-api/graphql/graphql_client.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var _endpoint : String = "" setget set_endpoint
+
+
+#-------------------------------------------------------------------------------
+func set_endpoint(value : String) -> void:
+	_endpoint = value
+
+
+#-------------------------------------------------------------------------------
